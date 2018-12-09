6c48d47beec483cbddfa8ce5bdcd3f3f71d15463
haresh223 <hareshmca@gmail.com>
2018-12-09 01:01:56 -0600

    Adding carmain java file

---
+++ Practice/basics/src/main/java/CarMain.java
@@ -0,0 +1,18 @@
+import com.basics.haresh.practice.Car;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car c = new Car();        // Create a new object and c(object veriabe name) and  custome made data type is Car , object name and class shoud be same, new car contains model, tyoe, year, colour
+        c.setModel("BMW");
+        c.setType("Sedan");
+        c.setYear(2014);
+        c.setColour("White");
+        c.carDetails();
+
+        Car c1 = new Car();
+        c1.carDetails();
+        System.out.println(c1);
+        Car c2 = new Car("Benz", "sedan", 2010, "white");
+        System.out.println(c2);
+    }
+}
