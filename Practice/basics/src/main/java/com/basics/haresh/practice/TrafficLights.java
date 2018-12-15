2aac1e5a7204e612e2e42dc47520f5a2c4857136
haresh223 <hareshmca@gmail.com>
2018-12-15 16:22:19 -0600

    ReverseArray, Datastructures

--- Practice/basics/src/main/java/com/basics/haresh/practice/TrafficLights.java
+++ Practice/basics/src/main/java/com/basics/haresh/practice/TrafficLights.java
@@ -1,30 +1,35 @@
 package com.basics.haresh.practice;
-
 public class TrafficLights {
-        public static void main(String args[]){
+    public static void main(String args[]) {
 
-            String color="RED";
-            switch(color) {
-                case "Green":
-                    System.out.println("You can go in green");
-                    break;
-                case "Yellow":
-                    System.out.println("You need to slowdown");
-                    break;
-                case "RED":
-                    System.out.println("You must stop at RED signal");
-                    break;
-                default:
-                    System.out.println("No signal installed");
-                    break;
-            }
+        String array[] = {"RED", "GREEN", "RED", "AMBER", "GREEN", "RED"};
+        for (String x : array) {
+            System.out.println(x);
         }
     }
+}
 
 
-//    RED
-// GREEN
-//            AMBER
-//RED
-//                    GrEEN
-//AMBER
+
+//package com.basics.haresh.practice;
+//
+//public class TrafficLights {
+//    public static void main(String args[]){
+//
+//        String color="RED";
+//        switch(color) {
+//            case "Green":
+//                System.out.println("You can go in green");
+//                break;
+//            case "Yellow":
+//                System.out.println("You need to slowdown");
+//                break;
+//            case "RED":
+//                System.out.println("You must stop at RED signal");
+//                break;
+//            default:
+//                System.out.println("No signal installed");
+//                break;
+//        }
+//    }
+//}
